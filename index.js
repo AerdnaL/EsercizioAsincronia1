e9b0dfc02857da1c56d8b68b12c432feca9418b6
AerdnaL <39456876+AerdnaL@users.noreply.github.com>
2023-08-08 15:46:52 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 // Chiave: 7a282772
 
+const base =
+  'https://eu-central-1.aws.data.mongodb-api.com/app/kvaas-giwjg/endpoint';
+const op = 'get';
+const key = '7a282772';
+const URL = base + '/' + op + '?key=' + key;
+
+
+
+/*const db = (URL) => { non funge
+  fetch(URL)
+    .then(
+      (response) => response.json(),
+      (error) => alert(error)
+    )
+    .then((data) => {
+      const bo = JSON.parse(data);
+      console.log(data);
+      return bo;
+    });
+};
+
+console.log(db);*/
+
+/*fetch(URL)
+  .then(
+    (response) => response.json(),
+    (error) => alert(error)
+  )
+  .then((data) => {
+    const db = JSON.parse(data);
+    console.log(db);
+  });*/
+
 class Biblioteca {
   constructor() {
     this.elencoLibri = [];
@@ -29,14 +62,16 @@ class Biblioteca {
 }
 
 const Biblioteca1 = new Biblioteca();
-Biblioteca1.elencoLibri = [
+/*Biblioteca1.elencoLibri = [
   { autore: 'Dante Alighieri', titolo: 'La Divina Commedia' },
   { autore: 'Dante Alighieri', titolo: 'De Vulgari Eloquentia' },
   { autore: 'Alessandro Manzoni', titolo: 'I Promessi Sposi' },
   { autore: 'Luigi Pirandello', titolo: 'Il Fu Mattia Pascal' },
   { autore: 'Luigi Pirandello', titolo: 'Uno, Nessuno, Centomila' },
   { autore: 'Luigi Pirandello', titolo: 'Mal Giocondo' },
-];
+];*/
+
+Biblioteca1.elencoLibri;
 
 const inputTesto = document.getElementById('inputTesto');
 const spazioRisultati = document.getElementById('spazioRisultati');
